5df4ed1be0647ff24f072cd76db06820b367d077
Rodoj <Rodoj@RODOPC>
2025-04-28 21:37:23 -0600

    Agregar archivos de proyecto.

---
+++ prelabRJULVC36/Form2.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prelabRJULVC36
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        int codigo1 = 1;
+
+        void limpiar()
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            textBox6.Text = "";
+            textBox7.Text = "";
+            textBox8.Text = "";
+            textBox9.Text = "";
+            textBox10.Text = "";
+            textBox11.Text = codigo1.ToString();
+
+
+        }
+        
+          
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewCell cell in dataGridView1.SelectedCells)
+            {
+                cell.Value = null; 
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            object[] agregar =
+            {
+               
+                textBox1 .Text,
+                textBox2 .Text,
+                textBox3 .Text,
+                textBox4 .Text,
+                textBox5 .Text,
+                textBox6 .Text,
+                textBox7 .Text,
+                textBox8 .Text,
+                textBox9 .Text,
+                textBox10 .Text,
+                textBox11 .Text,
+
+
+            };
+            dataGridView1.Rows.Add(agregar);
+            MessageBox.Show("Dato Registrado", "Mensaje");
+            codigo1++;
+            textBox11.Text = codigo1.ToString();
+            limpiar();
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.Rows.Count != 0)
+            {
+                int indice = dataGridView1.CurrentRow.Index;
+                dataGridView1.Rows[indice].Cells[0].Value = textBox1.Text;
+                dataGridView1.Rows[indice].Cells[1].Value = textBox2.Text;
+                dataGridView1.Rows[indice].Cells[2].Value = textBox3.Text;
+                dataGridView1.Rows[indice].Cells[3].Value = textBox4.Text;
+                dataGridView1.Rows[indice].Cells[4].Value = textBox5.Text;
+                dataGridView1.Rows[indice].Cells[5].Value = textBox6.Text;
+                dataGridView1.Rows[indice].Cells[6].Value = textBox7.Text;
+                dataGridView1.Rows[indice].Cells[7].Value = textBox8.Text;
+                dataGridView1.Rows[indice].Cells[8].Value = textBox9.Text;
+                dataGridView1.Rows[indice].Cells[9].Value = textBox10.Text;
+                dataGridView1.Rows[indice].Cells[10].Value = textBox11.Text;
+                MessageBox.Show("Dato Modificado");
+
+            }
+
+
+
+        }
+
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView1.RowCount != 0)
+            {
+                int fila = dataGridView1.CurrentCell.RowIndex;
+                textBox1.Text = dataGridView1[0, fila ].Value.ToString();
+                textBox2.Text = dataGridView1[1, fila].Value.ToString();
+                textBox3.Text = dataGridView1[2, fila].Value.ToString();
+                textBox4.Text = dataGridView1[3, fila].Value.ToString();
+                textBox5.Text = dataGridView1[4, fila].Value.ToString();
+                textBox6.Text = dataGridView1[5, fila].Value.ToString();
+                textBox7.Text = dataGridView1[6, fila].Value.ToString();
+                textBox8.Text = dataGridView1[7, fila].Value.ToString();
+                textBox9.Text = dataGridView1[8, fila].Value.ToString();
+                textBox10.Text = dataGridView1[9, fila].Value.ToString();
+                textBox11.Text = dataGridView1[10, fila].Value.ToString();
+            }
+        }
+
+        private void textBox11_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Form3 form3 = new Form3();
+            form3.Show();
+
+        }
+    }
+}
---
+++ prelabRJULVC36/Form3.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace prelabRJULVC36
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(146, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(465, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Rodrigo Javier Urizar López VC 36";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::prelabRJULVC36.Properties.Resources.Imagen_de_WhatsApp_2025_04_28_a_las_17_19_57_d62e740e;
+            this.pictureBox1.Location = new System.Drawing.Point(184, 111);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(399, 251);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
